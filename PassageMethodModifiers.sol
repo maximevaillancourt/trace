72786b41c4c216fc96b0345534d9bc5cc3e16cad
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2018-02-06 11:28:43 -0500

    Add gitignore and base Ownership files

---
+++ PassageMethodModifiers.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.20;
+
+contract PassageMethodModifiers is Ownable {
+    // Required modifiers:
+    // OnlyPassageTeam
+    // OnlyIndustry
+    // OnlyPublic
+    // OnlyGovernment
+    
+}
