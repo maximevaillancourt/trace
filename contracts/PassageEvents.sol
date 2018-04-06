3eba42468bb7915d87165701f0740c1ba8a634c4
Maxime Vaillancourt <maxvaillancourt1@gmail.com>
2018-04-05 20:01:42 -0400

    set 'owner' parameter as 'indexed' in 'ProductCreated' event

--- contracts/PassageEvents.sol
+++ contracts/PassageEvents.sol
@@ -4,7 +4,7 @@ import "./PassageModel.sol";
 
 contract PassageEvents is PassageModel {
 
-    event ProductCreated(bytes32 newProductId, address owner);
+    event ProductCreated(bytes32 newProductId, address indexed owner);
     // event OwnershipTransferRequest(uint productId, address indexed currentOwner, address indexed newRequestedOwner);
     // event ProductDetailsUpdated(uint productId);
     // event ProductMarkedAsSold(uint productId);
